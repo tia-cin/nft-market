72e80c3447cf808494cc220348807f9dcbece44c
Cintia Arce <cintiaayelenarce@gmail.com>
2022-10-12 10:14:04 -0300

    components, assets & constants

--- App.tsx
+++ App.tsx
@@ -1,9 +1,28 @@
-import { StyleSheet, Text, View } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import Home from "./screens/Home";
+import Details from "./screens/Details";
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const theme = {
+  ...DefaultTheme,
+  colors: { ...DefaultTheme.colors, background: "transparent" },
+};
+
+const App = () => {
   return (
-    <View>
-      <Text>Hello!</Text>
-    </View>
+    <NavigationContainer theme={theme}>
+      <Stack.Navigator
+        screenOptions={{ headerShown: false }}
+        initialRouteName="Home"
+      >
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Details" component={Details} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
+
+export default App;
---
+++ screens/Home.tsx
@@ -0,0 +1,10 @@
+import { View, Text, SafeAreaView, FlatList } from "react-native";
+import React, { useState } from "react";
+import { COLORS, NFTData } from "../constants";
+import { NFTCard, HomeHeader, FocusStatusBar } from "../components";
+
+const Home = () => {
+  return <SafeAreaView style={{ flex: 1 }}></SafeAreaView>;
+};
+
+export default Home;
