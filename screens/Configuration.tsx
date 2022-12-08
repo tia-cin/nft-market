8de99fa93c4ebb6cf3a5212048ff238502b53687
cintia arce <cintiaayelenarce@gmail.com>
2022-12-08 17:47:48 -0300

    new files

---
+++ screens/Configuration.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Configuration = () => {
+  return (
+    <View>
+      <Text>Configuration</Text>
+    </View>
+  )
+}
+
+export default Configuration
