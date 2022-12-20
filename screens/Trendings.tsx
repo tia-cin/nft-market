ceae1860a9ef4f5cc6afa5948149cc0a94be2cea
cintia arce <cintiaayelenarce@gmail.com>
2022-12-20 19:24:16 -0300

    Notification & Trendings Screen

---
+++ screens/Trendings.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Trendings = () => {
+  return (
+    <View>
+      <Text>Trendings</Text>
+    </View>
+  );
+};
+
+export default Trendings;
--- screens/index.ts
+++ screens/index.ts
@@ -6,5 +6,7 @@ import User from "./User";
 import CreateNFT from "./CreateNFT";
 import { Bids, LikedNFTs, UserNFTs } from "./UserPages";
 import AddBid from "./AddBid";
+import Notification from "./Notification";
+import Trendings from "./Trendings";
 
-export { Home, Landing, Auth, User, Details, CreateNFT, Bids, LikedNFTs, UserNFTs, AddBid };
+export { Home, Landing, Auth, User, Details, CreateNFT, Bids, LikedNFTs, UserNFTs, AddBid, Notification, Trendings };
