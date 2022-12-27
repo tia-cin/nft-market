fb52243fee4d95b44ee09897b482e1276322c02f
cintia arce <cintiaayelenarce@gmail.com>
2022-12-27 14:50:16 -0300

    userSettings picture

--- screens/UserSettings.tsx
+++ screens/UserSettings.tsx
@@ -1,24 +1,58 @@
 import { View, Text, SafeAreaView, Image } from "react-native";
 import React from "react";
 import { FocusStatusBar, Header, Inputs, RectButton } from "../components";
-import { assets, COLORS } from "../constants";
+import { assets, COLORS, SIZES } from "../constants";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { userInfo } from "../constants/dummy";
 
 const UserSettings = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.primary }}>
       <FocusStatusBar background={COLORS.primary} />
       <Header text="Edit profile" />
-      <View>
-        <View>
+      <View
+        style={{
+          backgroundColor: COLORS.secondary,
+          borderTopStartRadius: SIZES.extraLarge,
+          flex: 1,
+          padding: SIZES.medium,
+        }}
+      >
+        <View style={{ flexDirection: "row", marginVertical: SIZES.medium }}>
           <View>
-            <Image source={assets.person01} />
-            <Ionicons name="pencil-outline" />
+            <Image
+              source={assets.person01}
+              style={{ width: 100, height: 100 }}
+            />
+            <Ionicons
+              size={50}
+              color={COLORS.white}
+              name="pencil-outline"
+              style={{
+                position: "absolute",
+                top: 20,
+                left: 25,
+              }}
+            />
           </View>
-          <View>
-            <Text>Please choose an image of at least 800x800px</Text>
-            <RectButton text="Remove" />
+          <View style={{ width: 300, paddingHorizontal: SIZES.medium }}>
+            <Text
+              style={{
+                color: COLORS.light,
+                fontSize: SIZES.medium,
+                paddingBottom: SIZES.base,
+              }}
+            >
+              Please choose an image of at least 800x800px
+            </Text>
+            <RectButton
+              text="Remove"
+              props={{
+                backgroundColor: "transparent",
+                borderWidth: 1,
+                borderColor: COLORS.white,
+              }}
+            />
           </View>
         </View>
         <View>
