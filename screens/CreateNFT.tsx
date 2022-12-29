a4ec8aa410be85a5a4c5858da74e0115f2f6684d
cintia arce <cintiaayelenarce@gmail.com>
2022-12-29 19:58:43 -0300

    develop branch

--- screens/CreateNFT.tsx
+++ screens/CreateNFT.tsx
@@ -55,59 +55,59 @@ const CreateNFT = () => {
     );
   };
   return (
-    <Gallery />
-    // <SafeAreaView
-    //   style={{
-    //     flex: 1,
-    //     padding: SIZES.base,
-    //     backgroundColor: COLORS.primary,
-    //   }}
-    // >
-    //   <FocusStatusBar background={COLORS.primary} />
-    //   <Header text="Create your NFT" menu />
-    //   <View
-    //     style={{
-    //       flex: 5,
-    //       paddingHorizontal: SIZES.medium,
-    //       justifyContent: "space-evenly",
-    //     }}
-    //   >
-    //     <Inputs value={nftName} onChange={setNftName} />
-    //     <Inputs value={nftPrice} onChange={setNftPrice} />
-    //     <Inputs
-    //       value={nftDesc}
-    //       onChange={setNftDesc}
-    //       props={{ multiline: true, numberOfLines: 5 }}
-    //     />
-    //     <Inputs
-    //       value={nftFile}
-    //       onChange={setNftFile}
-    //       props={{ multiline: true, numberOfLines: 5 }}
-    //     />
-    //   </View>
-    //   <View style={{ flex: 1.5 }}>
-    //     <RectButton
-    //       text="Submit"
-    //       props={{
-    //         backgroundColor: COLORS.secondary,
-    //         marginHorizontal: SIZES.medium,
-    //       }}
-    //       handlePress={() => {
-    //         // saveFile();
-    //         NFTData.push({
-    //           id: "NFT-20",
-    //           name: nftName,
-    //           creator: "Full Name",
-    //           price: Number(nftPrice),
-    //           description: nftDesc,
-    //           image: "",
-    //           bids: [],
-    //           like: false,
-    //         });
-    //       }}
-    //     />
-    //   </View>
-    // </SafeAreaView>
+    // <Gallery />
+    <SafeAreaView
+      style={{
+        flex: 1,
+        padding: SIZES.base,
+        backgroundColor: COLORS.primary,
+      }}
+    >
+      <FocusStatusBar background={COLORS.primary} />
+      <Header text="Create your NFT" menu />
+      <View
+        style={{
+          flex: 5,
+          paddingHorizontal: SIZES.medium,
+          justifyContent: "space-evenly",
+        }}
+      >
+        <Inputs value={nftName} onChange={setNftName} />
+        <Inputs value={nftPrice} onChange={setNftPrice} />
+        <Inputs
+          value={nftDesc}
+          onChange={setNftDesc}
+          props={{ multiline: true, numberOfLines: 5 }}
+        />
+        <Inputs
+          value={nftFile}
+          onChange={setNftFile}
+          props={{ multiline: true, numberOfLines: 5 }}
+        />
+      </View>
+      <View style={{ flex: 1.5 }}>
+        <RectButton
+          text="Submit"
+          props={{
+            backgroundColor: COLORS.secondary,
+            marginHorizontal: SIZES.medium,
+          }}
+          handlePress={() => {
+            // saveFile();
+            NFTData.push({
+              id: "NFT-20",
+              name: nftName,
+              creator: "Full Name",
+              price: Number(nftPrice),
+              description: nftDesc,
+              image: "",
+              bids: [],
+              like: false,
+            });
+          }}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
